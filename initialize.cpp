96154a0cd30c76c0ea7249504cb8369390636dda
Danny Nguyen <dannyqdvd@yahoo.com>
2015-04-15 22:01:39 -0500

    first commit

---
+++ initialize.cpp
@@ -0,0 +1,26 @@
+global float master [4][4];
+
+struct people{
+	float r_munsi[2];
+	float r_danny[2];
+	float r_Munsi[2];
+	float r_Munsi[2];
+	
+
+}munsi, danny, gavin, preston;
+
+void main(){
+
+	//initalize master
+	initialize();
+}
+
+void initalize(){
+
+	//initialize master array
+	//TODO: pull from txt file later
+	master[][] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+	//initalize people
+
+}
